e0b28139fea19ed0d4d54a987334b1c15e0b1a05
Jonatan Andres <andreess891@gmail.com>
2016-09-09 18:29:49 -0500

    sadasd

---
+++ Restaurants/Views/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  Restaurants
+//
+//  Created by Jonatan Londoño Taborda on 9/09/16.
+//  Copyright © 2016 JaLt. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
